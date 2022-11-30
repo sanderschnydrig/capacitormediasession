ab56364e660d4ed7d3eac694ec1da32784097769
Sander Schnydrig <s.schnydrig@mengisgruppe.ch>
2022-11-30 09:42:08 +0100

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'mediasession',
+  webDir: 'dist/mediasession',
+  bundledWebRuntime: false
+};
+
+export default config;
